516cd7addcf767380b7b3f8c0cbb79064c09753a
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-05-29 12:15:42 -0400

    Added energy

--- school.js
+++ school.js
@@ -1,5 +1,6 @@
 let school = new Object();
 
+
 school.address = "630 Ninth Ave, Ste 901, New York, NY 10036"; 
 school.phone = "646-215-2200";
 school.hours = "09:30am-09:00pm";
@@ -7,11 +8,12 @@ school.website = "Codeimmersives.com";
 school.fax = "917-398-9853";
 
 class Member {
-    constructor(firstName, lastName, term, position){
+    constructor(firstName, lastName, term, position, energy){
         this.firstName = firstName;
         this.lastName = lastName;
         this.term = term;
         this.position = position;
+        this.energy = energy;
     }
 
     break(amount) {
@@ -31,13 +33,15 @@ class Member {
 
 }
 
-const miguel = new Member('Miguel', 'Fernandez', 'term2', 'Student');
-const yuri = new Member('Yuri', 'Shkoda', 'term2', 'Instructor');
-const jimmy = new Member('Jimmy', '', 'term2', 'Student')
+const miguel = new Member('Miguel', 'Fernandez', 'term2', 'Student', 75);
+const yuri = new Member('Yuri', 'Shkoda', 'term2', 'Instructor', 75);
+const jimmy = new Member('Jimmy', '', 'term2', 'Student', 75);
 
 miguel.lunch(45);
 yuri.coding(60);
 jimmy.break(200);
+
+console.log("Jimmy's Energy is:", jimmy.energy);
 
 
 console.log('School Information:', school);
