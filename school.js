4d24c4a9b0bb6abd2ea2f4cf1ba35e207f8f4020
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-05-29 11:58:11 -0400

    School Prototype

---
+++ school.js
@@ -0,0 +1,38 @@
+let school = new Object();
+
+school.address = "630 Ninth Ave, Ste 901, New York, NY 10036"; 
+school.phone = "646-215-2200";
+school.hours = "09:30am-09:00pm";
+school.website = "Codeimmersives.com";
+school.fax = "917-398-9853";
+
+class Member {
+    constructor(firstName, lastName, term, position){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.term = term;
+        this.position = position;
+    }
+
+    break(amount) {
+        console.log(`${this.position} ${this.firstName} is on break.`)
+        this.energy += amount
+    }
+
+    lunch(amount) {
+        console.log(`${this.position} ${this.firstName} is eating.`)
+        this.energy += amount
+    }
+
+    coding(amount) {
+        console.log(`${this.position} ${this.firstName} is coding.`)
+        this.energy -= amount
+    }
+
+}
+
+const miguel = new Member('Miguel', 'Fernandez', 'term2', 'student');
+const yuri = new Member('Yuri', 'Shkoda', 'term2', 'instructor');
+
+miguel.lunch(45);
+yuri.coding(60);
